da565195bbbed3fddf74772808286c902b0e2dfb
Liliane Wiemes <liliane_wiemes@hotmail.com>
2019-02-21 17:47:51 -0300

    Ajuste configuração do banco de dados

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == 'production') {
-  module.exports = { mongoURI: 'mongodb://liliane:123456l@ds163764.mlab.com:63764/blogapp-prod' }
+  module.exports = { mongoURI: 'mongodb://liliane:123456l@ds147125.mlab.com:47125/sis-patrimonio-prod' }
 } else {
-  module.exports = { mongoURI: 'mongodb://localhost/blogapp' }
+  module.exports = { mongoURI: 'mongodb://localhost/patrimonio' }
 }
